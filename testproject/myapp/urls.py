f9a40215e55c4b9c18f7637cf65294ce6308800d
witaya007 <witaya.chaison@gmail.ccom>
2025-03-17 00:03:52 +0700

    change location url

--- testproject/myapp/urls.py
+++ testproject/myapp/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,re_path, include
-
+from django.urls import path ,re_path
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(('myapp.urls', 'myapp'), namespace='myapp')),
+    
 
-  
+    path('', views.witaya),
+    path('hello/<int:id>', views.hello),
+    # add repath with year and slug
+    # add repath with year and slug
+    re_path(r'article/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)', views.article)  
+    
 ]
