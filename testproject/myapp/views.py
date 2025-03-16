0ba49692eef5867788fa4fc95873acb27eb205a3
witaya007 <witaya.chaison@gmail.ccom>
2025-03-16 21:57:52 +0700

    add parameter

--- testproject/myapp/views.py
+++ testproject/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def hello(request):
+def witaya(request):
     return HttpResponse("Witaya chaison")
+
+def hello(request , id):
+    # add hello with id to the response     
+    return HttpResponse(f"Hello, {id}")
+
+    # return HttpResponse("Witaya chaison")
--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -20,6 +20,7 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
+    path('witaya/', views.witaya),
+    path('hello/<int:id>', views.hello),
 
 ]
